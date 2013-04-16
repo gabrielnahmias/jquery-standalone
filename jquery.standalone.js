ff6f864fa47e423430c311a786428909aa676aa9
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-04-16 11:59:58 -0500

    v1.0.1:   Added capability to have options.   Made spacer feature a separate DIV instead of a CSS enhancement of the BODY (to preserve CSS and ensure proper alignment), along with an option pertaining to whether this is enabled.   Changed HTML element class modification to raw JavaScript.   Added main execution area to go along with new functionality.   A few other changes here and there (condensed code).

--- jquery.standalone.js
+++ jquery.standalone.js
@@ -1,5 +1,5 @@
 /*!
- * jquery-standalone v1.0.0
+ * jquery-standalone v1.0.1
  * http://github.com/terrasoftlabs/jquery-standalone
  *
  * Copyright © 2012 Gabriel Nahmias.
@@ -10,34 +10,69 @@
 
 ( function ($) {
 	
-	if ( window.navigator.userAgent.match(/iphone/gi) != null ) {
-		
-		// Might as help indicate if we're on an iPhone or not.
-		
-		$.browser.iphone = true;
-		
-		if ( ("standalone" in window.navigator) && !window.navigator.standalone ) {
+	var that = this,
+	
+	// Constants:
+		CLASS = "iphone-standalone";
+	
+	if (!$.browser)
+		$.browser = {};
+	
+	$.browser.__standaloneOn = false;
+	
+	var methods = {
+		toggle: function(oParams) {
 			
-			$.browser.standalone = false;
+			this.oSettings = $.extend( {
+				spacer: true
+			}, oParams);
 			
-		} else {
-			
-			$.browser.standalone = true;
-			
-			// Add a class to the body indicating we're on an iPhone and it's a web application
-			// to facilitate custom styling, etc.
-			
-			$("html").addClass("iphone-standalone");
-			
+			if ($.browser.__standaloneOn) {
+				$("."+CLASS+".spacer").remove();
+				document.documentElement.className.replace( ( ( document.documentElement.className.split(" ").length <= 1 ) ? "" : " ") + CLASS , "");
+				$.browser.__standaloneOn = false;
+			} else {
+				if ( window.navigator.userAgent.match(/iphone/gi) != null ) {
+					
+					// Might as well help indicate if we're on an iPhone or not.
+					$.browser.iphone = true;
+					
+					if ( ("standalone" in window.navigator) && !window.navigator.standalone ) {
+						$.browser.standalone = false;
+					} else {
+						$.browser.standalone = true;
+						
+						// Add a class to the root element indicating we're on an iPhone and it's a web application
+						// to facilitate custom styling, etc.
+						document.documentElement.className +=
+						((document.documentElement.className == "") ? "" : " ") + CLASS;
+						
+						if (this.oSettings.spacer) {
+							// Optionally, add a small spacer to the top of the page to prevent page content
+							// from being under the iPhone status bar.
+							$("body").prepend('<div class='+CLASS+' spacer"></div>').css("padding-top", "20px");
+						}
+						
+					}
+					$.browser.__standaloneOn = true;
+					
+				} else {
+					// No go.
+					$.browser.iphone = false;
+					$.browser.standalone = false;
+				}
+			}
 		}
-		
-	} else {
-		
-		// No go.
-		
-		$.browser.iphone = false;
-		$.browser.standalone = false;
-		
 	}
+		
+	$.standalone = function(method) {
+		// Main execution.
+		if ( methods[method] )
+			return methods[method].apply( this, Array.prototype.slice.call(arguments, 1) );
+		else if (typeof method === 'object' || !method)
+			return methods['toggle'].apply(this, arguments);
+		else
+			$.error('Method ' + method + ' does not exist in jQuery.standalone.');
+	};
 	
 } )(jQuery);
